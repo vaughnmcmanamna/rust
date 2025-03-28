d87bf47d12438c0f1d507d99603e6141553c9cf1
vaughnmcmanamna <vaughnmcmanamna@uvic.ca>
2025-03-28 01:53:25 -0700

    strucs n stuff

---
+++ chap5/target/debug/chap5.d
@@ -0,0 +1,1 @@
+/home/vaughnmcmanamna/Documents/csc330/textbook/rust/chap5/target/debug/chap5: /home/vaughnmcmanamna/Documents/csc330/textbook/rust/chap5/src/main.rs
---
+++ chap5/target/debug/deps/chap5-1e2ff0ec09c18a26.d
@@ -0,0 +1,5 @@
+/home/vaughnmcmanamna/Documents/csc330/textbook/rust/chap5/target/debug/deps/chap5-1e2ff0ec09c18a26: src/main.rs
+
+/home/vaughnmcmanamna/Documents/csc330/textbook/rust/chap5/target/debug/deps/chap5-1e2ff0ec09c18a26.d: src/main.rs
+
+src/main.rs:
